a445e0df16ec2cd8fb8af20bd000942e5a0e7615
Michael Wirth <mw@unknown0023dfa4f2e6.home>
2012-05-03 23:10:19 +0200

    Added fixes. First working version.

--- marrow/mailer/transport/sendgrid.py
+++ marrow/mailer/transport/sendgrid.py
@@ -1,11 +1,13 @@
+tasks.send_email({'to': "michael.wirth@dealini.ch", 'to_name': u"Michäel Wirth", 'subject': u"teäst"}, {'message': "asdfaffffasdf"}, "test")
 # encoding: utf-8
 
 import urllib
 
+from marrow.mailer.exc import MailConfigurationException, DeliveryFailedException, MessageFailedException
+
 __all__ = ['SendgridTransport']
 
 log = __import__('logging').getLogger(__name__)
-
 
 
 class SendgridTransport(object):
@@ -25,28 +27,26 @@ class SendgridTransport(object):
         # Sendgrid doesn't accept CC over the api
         if message.cc:
             to.extend(message.cc)
-            tasks.send_email({'to': "michael.wirth@dealini.ch", 'to_name': u"Michäel Wirth", 'subject': u"teäst"}, {'message': "asdfaffffasdf"}, "test")
+
         args = dict({
                 'api_user': self.user,
                 'api_key': self.key,
-                'from': message.author.address.encode(),
-                'to': [toaddr.address.encode() for toaddr in to],
-                'toname': [toaddr.name.encode() for toaddr in to],
-                'subject': message.subject,
-                'text': message.plain
+                'from': [fromaddr.address.encode(message.encoding) for fromaddr in message.author],
+                'fromname': [fromaddr.name.encode(message.encoding) for fromaddr in message.author],
+                'to': [toaddr.address.encode(message.encoding) for toaddr in to],
+                'toname': [toaddr.name.encode(message.encoding) for toaddr in to],
+                'subject': message.subject.encode(message.encoding),
+                'text': message.plain.encode(message.encoding)
             })
 
-        if message.author.name:
-            args['fromname'] = message.author.name
-        
         if message.bcc:
-            args['bcc'] = [bcc.address.encode() for bcc in message.bcc]
+            args['bcc'] = [bcc.address.encode(message.encoding) for bcc in message.bcc]
         
         if message.reply:
-            args['replyto'] = message.reply.address.encode()
+            args['replyto'] = message.reply.address.encode(message.encoding)
         
         if message.rich:
-            args['html'] = message.rich
+            args['html'] = message.rich.encode(message.encoding)
         
         if message.attachments:
             # Not implemented yet
@@ -64,17 +64,15 @@ class SendgridTransport(object):
             raise MailConfigurationException()
 
         try:
-            response = urllib.urlopen("https://sendgrid.com/api/mail.send.json?" + urllib.urlencode(args))
+            response = urllib.urlopen("https://sendgrid.com/api/mail.send.json?" + urllib.urlencode(args, True))
         except IOError:
             raise DeliveryFailedException(message, "Could not connect to Sendgrid.")
         else:
             respcode = response.getcode()
             if respcode >= 400 and respcode <= 499:
-                raise MessageFailedException()
+                raise MessageFailedException(response.read())
             elif respcode >= 500 and respcode <= 599:
                 raise DeliveryFailedException(message, "Sendgrid service unavailable.")
-
-        # curl -d 'to=destination@example.com&toname=Destination&subject=Example Subject&text=testingtextbody&from=info@domain.com&api_user=sendgridUsername&api_key=sendgridPassword' 
     
     def shutdown(self):
         pass
